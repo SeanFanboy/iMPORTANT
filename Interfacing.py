e0fb7d7863a0304cebab9f3247d997f384e76375
SeanFanboy <24109@burnside.school.nz>
2026-05-07 09:07:28 +1200

    Got the FK's to work together and output the correct data

--- Interfacing.py
+++ Interfacing.py
@@ -7,12 +7,12 @@ DB = "uhnime.db"
 def print_all():
     db = sqlite3.connect(DB)
     cursor = db.cursor()
-    query = "SELECT * FROM Anime;"
+    query = "SELECT Anime.Ranking, Anime.Name, Anime.Score, Genres.Genre_name, Anime.Episodes FROM Anime  JOIN Genres ON Anime.Genre_ID = Genres.ID;"  
     cursor.execute(query)
     answers = cursor.fetchall()
-    print("Rank  Name                                                   Score   Genre  Episodes")
+    print("Rank  Name                                                   Score     Genre        Episodes")
     for answer in answers:
-        print(f"{answer[0]:<6}{answer[1]:<55}{answer[2]:<10}{answer[3]:<8}{answer[4]}")
+        print(f"{answer[0]:<6}{answer[1]:<55}{answer[2]:<10}{answer[3]:<15}{answer[4]}")
     db.close
 
 print_all()
