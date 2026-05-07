c9de71da8c5b756667be4576c30945888c4f2116
SeanFanboy <24109@burnside.school.nz>
2026-05-08 10:03:29 +1200

    Got the sorting by genre stuff working

--- Interfacing.py
+++ Interfacing.py
@@ -18,7 +18,7 @@ def print_all():
 def print_all_seinen():
     db = sqlite3.connect(DB)
     cursor = db.cursor()
-    query = "SELECT Anime.Ranking, Anime.Name, Anime.Score, Genres.Genre_name, Anime.Episodes FROM Anime  JOIN Genres ON Anime.Genre_ID = Genres.ID WHEN Genre_ID = 1;"  
+    query = "SELECT Anime.Ranking, Anime.Name, Anime.Score, Genres.Genre_name, Anime.Episodes FROM Anime  JOIN Genres ON Anime.Genre_ID = Genres.ID WHERE Genre_ID = 1;"  
     cursor.execute(query)
     answers = cursor.fetchall()
     print("Rank  Name                                                   Score     Genre        Episodes")
@@ -29,7 +29,7 @@ def print_all_seinen():
 def print_all_romance():
     db = sqlite3.connect(DB)
     cursor = db.cursor()
-    query = "SELECT Anime.Ranking, Anime.Name, Anime.Score, Genres.Genre_name, Anime.Episodes FROM Anime  JOIN Genres ON Anime.Genre_ID = Genres.ID;"  
+    query = "SELECT Anime.Ranking, Anime.Name, Anime.Score, Genres.Genre_name, Anime.Episodes FROM Anime  JOIN Genres ON Anime.Genre_ID = Genres.ID WHERE Genre_ID = 2;"  
     cursor.execute(query)
     answers = cursor.fetchall()
     print("Rank  Name                                                   Score     Genre        Episodes")
@@ -40,7 +40,7 @@ def print_all_romance():
 def print_all_shonen():
     db = sqlite3.connect(DB)
     cursor = db.cursor()
-    query = "SELECT Anime.Ranking, Anime.Name, Anime.Score, Genres.Genre_name, Anime.Episodes FROM Anime  JOIN Genres ON Anime.Genre_ID = Genres.ID;"  
+    query = "SELECT Anime.Ranking, Anime.Name, Anime.Score, Genres.Genre_name, Anime.Episodes FROM Anime  JOIN Genres ON Anime.Genre_ID = Genres.ID WHERE Genre_ID = 3;"  
     cursor.execute(query)
     answers = cursor.fetchall()
     print("Rank  Name                                                   Score     Genre        Episodes")
@@ -51,7 +51,7 @@ def print_all_shonen():
 def print_all_fantasy():
     db = sqlite3.connect(DB)
     cursor = db.cursor()
-    query = "SELECT Anime.Ranking, Anime.Name, Anime.Score, Genres.Genre_name, Anime.Episodes FROM Anime  JOIN Genres ON Anime.Genre_ID = Genres.ID;"  
+    query = "SELECT Anime.Ranking, Anime.Name, Anime.Score, Genres.Genre_name, Anime.Episodes FROM Anime  JOIN Genres ON Anime.Genre_ID = Genres.ID WHERE Genre_ID = 4;"  
     cursor.execute(query)
     answers = cursor.fetchall()
     print("Rank  Name                                                   Score     Genre        Episodes")
@@ -62,7 +62,7 @@ def print_all_fantasy():
 def print_all_comedy():
     db = sqlite3.connect(DB)
     cursor = db.cursor()
-    query = "SELECT Anime.Ranking, Anime.Name, Anime.Score, Genres.Genre_name, Anime.Episodes FROM Anime  JOIN Genres ON Anime.Genre_ID = Genres.ID;"  
+    query = "SELECT Anime.Ranking, Anime.Name, Anime.Score, Genres.Genre_name, Anime.Episodes FROM Anime  JOIN Genres ON Anime.Genre_ID = Genres.ID WHERE Genre_ID = 5;"  
     cursor.execute(query)
     answers = cursor.fetchall()
     print("Rank  Name                                                   Score     Genre        Episodes")
@@ -73,7 +73,7 @@ def print_all_comedy():
 def print_all_slice_of_life():
     db = sqlite3.connect(DB)
     cursor = db.cursor()
-    query = "SELECT Anime.Ranking, Anime.Name, Anime.Score, Genres.Genre_name, Anime.Episodes FROM Anime  JOIN Genres ON Anime.Genre_ID = Genres.ID;"  
+    query = "SELECT Anime.Ranking, Anime.Name, Anime.Score, Genres.Genre_name, Anime.Episodes FROM Anime  JOIN Genres ON Anime.Genre_ID = Genres.ID WHERE Genre_ID = 6;"  
     cursor.execute(query)
     answers = cursor.fetchall()
     print("Rank  Name                                                   Score     Genre        Episodes")
@@ -87,21 +87,21 @@ while True:
     user_input = input("What genre would you like to explore?\n1. Seinen\n2. Romance\n3. Shonen\n4. Fantasy\n5. Comedy\n6. Slice Of Life\n7. Print all\n8. Close the database\n")
 
     if user_input == '1':
-        print_all_seinen  
+        print_all_seinen()  
     elif user_input == '2':
-        print('placeholder')
+        print_all_romance()
     elif user_input == '3':
-        print('placeholder')
+        print_all_shonen()
     elif user_input == '4':
-        print('placeholder')
+        print_all_fantasy()
     elif user_input == '5':
-        print('placeholder')
+        print_all_comedy()
     elif user_input == '6':
-        print('placeholder')
+        print_all_slice_of_life()
     elif user_input == '7':
         print_all()
     elif user_input =='8':
+        print("Database Closed")
         break
     else:
         print("That wasn't an option!")
-
